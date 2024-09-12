5508e10f42b45bbeafb3220a02db40a1bcb35375
Andres Netbook <andres.silva41202@gmail.com>
2024-09-11 23:53:25 -0300

    agregando funcionalidad de carga y guardado a los datos en 'LocalStorage'

--- renderer.js
+++ renderer.js
@@ -1,22 +1,30 @@
 const btnAddTarea = document.getElementById('add-tarea')
 const lista = document.getElementById('lista')
 
-btnAddTarea.addEventListener('click', ()=>{
-    const inputTarea = document.getElementById('input-tarea')
-    const textoInput = inputTarea.value
-
-    if (textoInput){
-        const nuevaTarea = document.createElement('li')
+// Función para crear una nueva Tarea
+function crearNuevaTarea (textoInput, completed = false){
+    const nuevaTarea = document.createElement('li')
         const textoTarea = document.createElement('span')
         textoTarea.textContent = textoInput
-        textoTarea.style.textDecoration = "none"
+        textoTarea.style.textDecoration = completed ? 'line-through' : 'none'
         textoTarea.setAttribute('class', 'linea')
         nuevaTarea.setAttribute('class', 'item')
         nuevaTarea.appendChild(textoTarea)
         
-        const btnMarcarTarea = document.createElement('input')
+        const btnMarcarTarea = marcarTarea(textoTarea, completed)
+        const btnEliminar = eliminarTarea(lista, nuevaTarea)
+        const btnEditarTarea = editarTarea(textoTarea, nuevaTarea, btnMarcarTarea, btnEliminar)
+
+        nuevaTarea.append(btnEditarTarea,btnEliminar,btnMarcarTarea)
+        lista.insertAdjacentElement('afterbegin', nuevaTarea)
+}
+
+// Función para Incluir Botón de Marcar Tareas y su funcionamiento
+function marcarTarea(textoTarea, completed = false) {
+    const btnMarcarTarea = document.createElement('input')
         btnMarcarTarea.type = 'checkbox'
         btnMarcarTarea.className = 'checkbox-tarea'
+        btnMarcarTarea.checked = completed
 
         btnMarcarTarea.addEventListener('click', ()=>{
             if (btnMarcarTarea.checked){
@@ -24,16 +32,25 @@ btnAddTarea.addEventListener('click', ()=>{
             }else{
                 textoTarea.classList.remove('tachado')
             }
+            guardarTareas();
         })
+    return btnMarcarTarea;
+}
 
-        const btnEliminar = document.createElement('button')
+//Función para Incluir Botón de Eliminar Tareas y su funcionamiento
+function eliminarTarea(lista, nuevaTarea) {
+    const btnEliminar = document.createElement('button')
         btnEliminar.innerHTML = '<i class="fas fa-trash-alt"></i>'
-        // btnEliminar.textContent = "Eliminar"
         btnEliminar.addEventListener('click', ()=>{
             lista.removeChild(nuevaTarea)
+            guardarTareas()
         })
+    return btnEliminar
+}
 
-        const btnEditarTarea = document.createElement('button')
+// Función para Incluir Botón de Editar Tarea y su funcionamiento
+function editarTarea(textoTarea, nuevaTarea, btnMarcarTarea, btnEliminar) {
+    const btnEditarTarea = document.createElement('button')
         btnEditarTarea.textContent = 'Editar'
         btnEditarTarea.addEventListener('click', ()=>{
             if (btnEditarTarea.textContent === 'Editar'){
@@ -64,13 +81,44 @@ btnAddTarea.addEventListener('click', ()=>{
                         nuevaTarea.replaceChild(btnEliminar,btnCancelar)
                         btnEditarTarea.textContent = 'Editar'
                         nuevaTarea.appendChild(btnMarcarTarea)
+                        guardarTareas()
                     }
                 }, { once: true });
             }
         })
+        return btnEditarTarea
+}
 
-        nuevaTarea.append(btnEditarTarea,btnEliminar,btnMarcarTarea)
-        lista.insertAdjacentElement('afterbegin', nuevaTarea)
+// Función para guardar las tareas en localStorage
+function guardarTareas() {
+    const tareasArreglo = [];
+    // Recorre todas las tareas visibles en el DOM
+    document.querySelectorAll('.item').forEach(tarea => {
+        tareasArreglo.unshift({
+            text: tarea.querySelector('span').textContent,  // Texto de la tarea
+            completed: tarea.querySelector('.checkbox-tarea').checked  // Estado de completado
+        });
+    });
+    // Guarda el array de tareas en localStorage
+    localStorage.setItem('tareas', JSON.stringify(tareasArreglo));
+}
+// Función para cargar las tareas desde localStorage
+function cargarTareas() {
+    const tareasArreglo = JSON.parse(localStorage.getItem('tareas')) || []
+    tareasArreglo.forEach(tarea => {
+        crearNuevaTarea(tarea.text ,tarea.completed)
+    })
+}
+
+window.addEventListener('DOMContentLoaded', cargarTareas)
+
+btnAddTarea.addEventListener('click', ()=>{
+    const inputTarea = document.getElementById('input-tarea')
+    const textoInput = inputTarea.value
+
+    if (textoInput){
+        crearNuevaTarea(textoInput)
         inputTarea.value = ""
+        guardarTareas()
     }
 })
